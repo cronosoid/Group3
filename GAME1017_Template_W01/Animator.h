b9ca54b70f6afe8ab5ed6354713e81432df4ca8a
Maxim <Maxim@DESKTOP-ECH05PF>
2020-06-07 22:20:31 -0400

    Description

--- GAME1017_Template_W01/Animator.h
+++ GAME1017_Template_W01/Animator.h
@@ -15,7 +15,11 @@ class Animation
 {
 public: // Methods
 	Animation(Uint32 maxFrames, Uint32 priority, Uint32 moveX, Uint32 moveY = 0, Uint32 startX = 0, Uint32 startY = 0, Uint32 framesFrequency = 8);
-
+	//priority - animation with the highest priority will be played in the end of the frame
+	//framesFrequency - how often the animation will change its` picture
+	//Move x - how much pixels on the right should be skipped to reach the next frame
+	//Move y - if the animation is face-dependent, means animation facing the right is not the same as the animation facing the left,
+	//then it will move on moveY pixels downward for the left face animations, and not move downward for the right face animations
 	Uint32 getMoveX() { return moveX; }
 	Uint32 getMoveY() { return moveY; }
 	Uint32 getStartX() { return startX; }
@@ -40,10 +44,11 @@ public: // Variables
 public: // Methods
 	Animator(PlatformPlayer* Player);
 	Uint32 getFace() { return faceSide; }
-	void setFace(Uint32 face) { face == 0 ? faceSide = 0 : faceSide = 1; }
-	void setNextAnimation(const std::string& type);
-	void playAnimation();
+	void setFace(Uint32 face) { face == 0 ? faceSide = 0 : faceSide = 1; }//Setting the face. 0 is right, 1 is left
+	void setNextAnimation(const std::string& type);//Make a request to play an animation with specific name in the end of the frame
+	void playAnimation();//Plays the animation with the highest priority in the "hill"
 	void addAnimation(const std::string& key, Uint32 maxFrames, Uint32 priority, Uint32 moveX, Uint32 moveY = 0, Uint32 startX = 0, Uint32 startY = 0, Uint32 framesFrequency = 8);
+	//Adding animation in the animations map, which can be easily played by calling setNextAnimation(*animation name*)
 private: // Variables
 	Uint32 faceSide;
 	Uint32 animFrame;
