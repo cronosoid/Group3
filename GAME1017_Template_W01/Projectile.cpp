40e16e8d8a4abb83332fc259c5b001e936d863dc
Maxim <Maxim@DESKTOP-ECH05PF>
2020-06-23 19:02:56 +0300

    Half-done anim

--- GAME1017_Template_W01/Projectile.cpp
+++ GAME1017_Template_W01/Projectile.cpp
@@ -2,13 +2,36 @@
 #include "Engine.h"
 #include "States.h"
 
-Projectile::Projectile(SDL_Rect s, SDL_FRect d, SDL_Renderer* r, SDL_Texture* t, double spd, int face, double damage) :
+Projectile::Projectile(SDL_Rect s, SDL_FRect d, SDL_Renderer* r, SDL_Texture* t, double spd, int face, double damage,
+	int framesFrequency, int maxFrames, int moveX, int moveY) :
 	Sprite(s, d, r, t), m_speed(spd) {
 	this->face = face;
 	this->m_damage = damage;
+
+	this->m_framesFrequency = framesFrequency;
+	this->m_maxFrames = maxFrames;
+	this->m_moveX = moveX;
+	this->m_moveY = moveY;
 }
 
 void Projectile::Update()
 {
 	m_dst.x += m_speed * face;
+	if (m_curFrame >= m_framesFrequency)
+	{
+		m_curFrame = 0;
+		m_curAnimFrame++;
+		if (m_curAnimFrame >= m_maxFrames)
+		{
+			m_curAnimFrame = 0;
+		}
+
+		m_src.x = m_moveX * m_curAnimFrame;
+		if (face == -1)
+			m_src.y = m_moveY;
+	}
+	else
+	{
+		m_curFrame++;
+	}
 }
